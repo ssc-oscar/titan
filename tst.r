72ac1dbd209f70a3a03bfcb49fc665adf491914b
Audris Mockus <audris@mockus.org>
2018-03-30 14:58:13 -0400

    calculate the full list of five measures

--- tst.r
+++ tst.r
@@ -2,6 +2,7 @@ suppressMessages(library(pbdMPI,quietly = TRUE))
 suppressMessages(library(pbdDMAT,quietly = TRUE))
 suppressMessages(library(pbdIO,quietly = TRUE))
 .libPaths('./R/x86_64-pc-linux-gnu-library/3.3')
+suppressMessages(library('data.table', quietly = TRUE))
 suppressMessages(library('RecordLinkage', lib.loc="./R/x86_64-pc-linux-gnu-library/3.3", quietly = TRUE))
 
 
