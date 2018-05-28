ec614947819a318631ca8c7ab64b825e568352d4
Audris Mockus <audris@mockus.org>
2018-05-28 11:59:30 -0400

    fixed output, ready for scale?

--- tst.r
+++ tst.r
@@ -5,6 +5,9 @@ suppressMessages(library(pbdIO,quietly = TRUE))
 suppressMessages(library('data.table', quietly = TRUE))
 suppressMessages(library('RecordLinkage', lib.loc="./R/x86_64-pc-linux-gnu-library/3.3", quietly = TRUE))
 
+FR = __FROM__;
+TO = __TO__;
+
 init()
 #Rprof(append = TRUE)
 
@@ -25,26 +28,29 @@ comm.print("read all");
 #comm.print(dxf, all.rank=TRUE)
 
 #tandem.webdev,Agence-Tandem,Agence-Tandem,Agence-Tandem,tandem.webdev@gmail.com,Agence-Tandem <tandem.webdev@gmail.com>
-#
-
-pairs = compare.linkage (x, x1, exclude=c(7),strcmp=c(1:6),strcmpfun = jarowinkler);
-barrier()
-comm.print("Computed pairs");
-#predict and write out matches
-MM=apply(pairs$pairs[,c("n", "e", "ln", "fn", "un", "ifn")],1,max, na.rm = T)>.8&pairs$pairs$id1 != pairs$pairs$id2;
-val = c();
-ll = sum(MM);
 myrank=comm.rank();
-fnamev=paste("outV",myrank,sep=".");
-if (ll > 0){
-  p = pairs$pairs[MM,-7];
-  comm.print(c(ll,dim(p)))
-  myrank=comm.rank();
-  a = rep(myrank,ll);
-  b = a;
-  val = cbind(a, b, p);
-  fwrite(data.frame(val),file=fnamev, sep=";",quote=FALSE);
-  rm(val)
+fnamev=paste("__OUT__/outV",myrank,sep=".");
+
+
+if (FR == 0){
+  pairs = compare.linkage (x, x1, exclude=c(7),strcmp=c(1:6),strcmpfun = jarowinkler);
+  barrier()
+  comm.print("Computed self pairs");
+  #predict and write out matches
+  MM=apply(pairs$pairs[,c("n", "e", "ln", "fn", "un", "ifn")],1,max, na.rm = T)>.8&pairs$pairs$id1 != pairs$pairs$id2;
+  val = c();
+  ll = sum(MM);
+  if (ll > 0){
+    p = pairs$pairs[MM,-9];
+    comm.print(c(ll,dim(p)))
+    a = rep(myrank,ll);
+    b = a;
+    p$a = a;
+    p$b = b
+    #val = data.frame(cbind(a, b, p));
+    fwrite(p,file=fnamev, sep=";",quote=FALSE,append=T);
+    rm(val)
+  }
 }
 
 message.pass <- function(off=1) {
@@ -64,28 +70,30 @@ message.get <- function(off=1) {
 }
 ncom = comm.size();
 nc = ceiling(comm.size()/2);
-for (i in 1:nc){
+
+for (i in max(1,FR):min(TO,nc)){
   message.pass(i);
   x1=message.get(i);
   pairs = compare.linkage (x, x1, exclude=c(7),strcmp=c(1:6),strcmpfun = jarowinkler);
   MM=apply(pairs$pairs[,c("n", "e", "ln", "fn", "un", "ifn")],1,max,na.rm = T)>.8;
   ll = sum(MM);
-  p = pairs$pairs[MM,-7];
+  p = pairs$pairs[MM,-9];
   comm.print(c(ll,dim(p)));
   if (ll > 0){
      orank = (myrank-i)%%ncom;
      a = rep(myrank,ll);
      b = rep(orank, ll);
-     val0 = cbind (a, b, p);
+     #val0 = cbind (a, b, p);
+     p$a=a;
+     p$b=b;
      #val = rbind(val, val0);
-     fwrite(data.frame(val0),file=fnamev, sep=";",quote=FALSE,append=T);
+     fwrite(p,file=fnamev, sep=";",quote=FALSE,append=T);
   }
 }
 
 #comm.print(lbl[1:10,], all.rank=TRUE)
 barrier();
 comm.print("Finished computing");
-myrank=comm.rank();
 ##fnamel=paste("outL",myrank,sep=".");
 ##fwrite(data.frame(lbl),file=fnamel, sep=";",quote=FALSE);
 #fnamev=paste("outV",myrank,sep=".");
