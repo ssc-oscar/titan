569c0ecfa1f45d66f5a4c1f0331fd9a104c9d43a
Audris Mockus <audris@mockus.org>
2018-06-19 11:45:10 -0400

    added prediction

--- pred.r
+++ pred.r
@@ -5,6 +5,8 @@ x=fread("4096_out/NNN.txt",sep=";",quote="",header=FALSE);
 x[is.na(x)]=0;
 names(x) = c("n0","o0","n1","o1","n","e","ln","fn","un", "ifn", "d2vSim");
 pr0 = predict(rfCr,x);
-fwrite(data.frame(n0=x[,"n0"],"o0"=x[,"o0"],n1=x[,"n1"],o1=x[,"o1"],pr=pr0),file="4096_out/NNN.p",sep=";")
+sel = pr0==1
+x = x[sel,c("n0", "o0","o0","n1","o1")];
+fwrite(x,file="4096_out/NNN.p",sep=";")
 
 
