8f1586cd61ed0b8c3e83f3e40d068ce2b7ccf749
Audris Mockus <audris@mockus.org>
2018-07-01 09:48:36 -0400

    doing with frequency

--- pred.r
+++ pred.r
@@ -3,7 +3,7 @@ load("rfmodelsCr.RData")
 library(data.table)
 x=fread("4096_out/NNN.txt",sep=";",quote="",header=FALSE);
 x[is.na(x)]=0;
-names(x) = c("n0","o0","n1","o1","n","e","ln","fn","un", "ifn", "d2vSim");
+names(x) = c("n0","o0","n1","o1","n","e","ln","fn","un", "ifn","fnf","lnf","unf", "d2vSim");
 pr0 = predict(rfCr,x);
 sel = pr0==1
 x = x[sel,c("n0","o0","n1","o1")];
